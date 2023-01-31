4beaca1ad11ef06b381ae863dcafd91bd6c2c68f
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2023-01-31 16:01:42 +0100

    Add first version of first tutorial notebook

---
+++ utils.py
@@ -0,0 +1,17 @@
+from matplotlib import colors
+import numpy as np
+from collections import OrderedDict
+
+CM_SM = colors.LinearSegmentedColormap.from_list('BrownBlue',
+    np.array([[134, 80, 16],
+            [164, 117, 13],
+            [219, 190, 24],
+            [250, 249, 156],
+            [144, 202, 240],
+            [4, 145, 251],
+            [8, 83, 211],
+            [13, 37, 161]]) / 255.)
+
+
+                
+    
