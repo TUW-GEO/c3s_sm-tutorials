1d6aece2f249461fdf5b5ba2b2048fc8bd08077e
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2023-01-31 17:06:11 +0100

    Export to rst

--- preRelease.sh
+++ preRelease.sh
@@ -1,3 +1,3 @@
 conda env export --no-builds | grep -v "prefix" > environment.yml
-jupyter nbconvert --to html T1_DataAccess\&Anomalies --output T1_DataAccess\&Anomalies
+jupyter nbconvert --to rst T1_DataAccess\&Anomalies --output T1_DataAccess\&Anomalies
 
