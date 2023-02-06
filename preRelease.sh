00d97941b98abae91665f2528598f081be221c8b
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2023-02-06 15:41:31 +0100

    Update notebook and html

--- preRelease.sh
+++ preRelease.sh
@@ -1,3 +1,3 @@
 #conda env export --no-builds | grep -v "prefix" > environment.yml
 black -l 99 *.ipynb
-jupyter nbconvert --to html_embed T1_DataAccess_Anomalies --output html/T1_DataAccess_Anomalies
+jupyter nbconvert --to html T1_DataAccess_Anomalies --output T1_DataAccess_Anomalies
