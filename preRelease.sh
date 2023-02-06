a180969c5efce49d4c12eb74574d2325294b30b7
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2023-02-06 14:02:40 +0100

    Update notebook and use pinned environment

--- preRelease.sh
+++ preRelease.sh
@@ -1,3 +1,3 @@
 #conda env export --no-builds | grep -v "prefix" > environment.yml
-black -l 79 *.ipynb
+black -l 99 *.ipynb
 jupyter nbconvert --to html_embed T1_DataAccess_Anomalies --output html/T1_DataAccess_Anomalies
